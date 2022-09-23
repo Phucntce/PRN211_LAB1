cdb7b6a9a3453b7bf829cc17e3eeeafbd1412942
thanhphuc <phucntce160284@fpt.edu.vn>
2022-09-23 15:59:13 +0700

    update

---
+++ People.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace PRN211_LAB1 {
+    class People {
+        public string ID_card { get; set; }
+        public string Full_name{ get; set; }
+        public string Address { get; set; }
+        public string Email { get; set; }
+    }
+}
