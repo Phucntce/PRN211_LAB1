638deeb4b31335786e5dd3e85cb2c43d555ae70d
thanhphuc <phucntce160284@fpt.edu.vn>
2022-09-25 21:39:48 +0700

    con ham main

--- Student.cs
+++ Student.cs
@@ -3,8 +3,8 @@ namespace PRN211_LAB1
 {
     class Student : People
     {
-        static public int size =10;
-        private int[] marks = new int[size];
+
+
         public Student() { }
         public Student(string id_card, string full_name, string birthday, string address, string email, string phone, string rollNo, string classNo)
         {
@@ -18,20 +18,17 @@ namespace PRN211_LAB1
             ClassNo = classNo;
 
         }
-        public int this[int index]
+        public Marks_Subject[] arr = new Marks_Subject[100];
+        public string this[int index]
         {
-            get { 
-                int tmp = 0;
-                if (index >= 0 && index < size - 1{
-                    tmp = marks[index];
-                }
-                return tmp;
-             }
-            set { 
-                if (index >= 0 && index < size - 1){
-                    marks[index] = value;
-                }
-             }
+            get
+            {
+                return arr[index].ToString();
+            }
+            set
+            {
+                arr[index].inputMarks();
+            }
         }
         public string RollNo { get; set; }
         public string ClassNo { get; set; }
@@ -43,16 +40,16 @@ namespace PRN211_LAB1
             ClassNo = CheckType.checkClassNo();
         }
 
-        public int calculateAvg(){
-            int sum = 0;
-            for (int i = 0; i < size - 1; i++){
-                sum+= marks[i];
-            }
-            return sum / size;
-        }
+        // public int calculateAvg()[
+        //     int sum = 0;
+
+        // ]
         public override void printInfo()
         {
-            
+            System.Console.WriteLine("Full Name: " + Full_name);
+            System.Console.WriteLine("Birthday: " + Birthday);
+            System.Console.WriteLine("RollNo: " + RollNo);
+            System.Console.WriteLine("ClassNo: " + ClassNo);
         }
 
     }
--- Teacher.cs
+++ Teacher.cs
@@ -4,7 +4,7 @@ namespace PRN211_LAB1
 {
     class Teacher : People
     {
-        private List<Subject> subject = new List<Subject>();
+
         public Teacher()
         {
 
@@ -21,25 +21,44 @@ namespace PRN211_LAB1
             TeacherCode = teacherCode;
             JoinDate = joinDate;
         }
-        public Subject this[int index]
+        Subject[] arr = new Subject[100];
+        public string this[int index]
         {
-            get { 
-                
-             }
-            set {  }
+            get
+            {
+                return arr[index].ToString();
+            }
+            set
+            {
+                arr[index].inputSubject();
+            }
         }
+        public DateTime Odate { get; set; }
         public string TeacherCode { get; set; }
         public string JoinDate { get; set; }
 
         public override void printInfo()
         {
-            
+            System.Console.WriteLine("Full Name: " + Full_name);
+            System.Console.WriteLine("Number Of Year Old: " + getAge());
+            System.Console.WriteLine("Teacher Code: " + TeacherCode);
+            System.Console.WriteLine("Joined Date: " + JoinDate);
         }
 
-        public void inputTeacher(){
+        public void inputTeacher()
+        {
             base.inputPeople();
             System.Console.Write("Enter teacher code: ");
             TeacherCode = CheckType.checkTeacherCode();
+            JoinDate = CheckType.checkBirthDay();
+            Odate = Convert.ToDateTime(JoinDate);
+        }
+
+
+        public int getWorkedYear()
+        {
+            var today = DateTime.Today;
+            return today.Year - Odate.Year;
         }
     }
 }
